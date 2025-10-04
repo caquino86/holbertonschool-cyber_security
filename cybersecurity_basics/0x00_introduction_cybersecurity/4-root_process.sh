c177083320b9ad8fece42ea14f7c650c7c25ae69
caquino86 <12019@holbertonstudents.com>
2025-10-04 18:42:25 -0400

    fix: remove unused code and improve user input handling in root_process.sh

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-#ps aux --no-heading | grep -v '^root' | awk -v user="$1" '$1 == user && ($5 != 0 || $6 != 0)'
-#!/bin/bash
-
-# Check if username is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <username>"
-  exit 1
-fi
-
-# Monitor processes started by the specified user
 ps -u "$1" -o user,pid,vsz,rss,comm --no-headers | grep -vE '^[^ ]+ +[0-9]+ +0 +0 '
