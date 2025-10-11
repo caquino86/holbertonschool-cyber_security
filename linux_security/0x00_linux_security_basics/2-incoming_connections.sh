7948b35d7fdc4ac8024aadd91b9fa9dc81b37467
caquino86 <12019@holbertonstudents.com>
2025-10-11 19:13:30 -0400

    fix: improve output formatting in iptables script

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf ""; sudo iptables -P INPUT DROP; sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+printf ".\n"; sudo iptables -P INPUT DROP; sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
