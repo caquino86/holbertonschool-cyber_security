04708f2728705dae4d1d66bf57d0d0cbc1b631a8
caquino86 <12019@holbertonstudents.com>
2025-10-11 19:12:04 -0400

    fix: remove unnecessary output from iptables rules application

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-printf "Aplicando reglas de iptables...\n"
-sudo iptables -P INPUT DROP
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+printf ""; sudo iptables -P INPUT DROP; sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
