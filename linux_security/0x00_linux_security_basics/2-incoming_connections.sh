2b851e8be880a1d7810df84bf35ce6cf4547be2f
caquino86 <12019@holbertonstudents.com>
2025-10-11 19:08:36 -0400

    fix: format iptables rules application for better readability

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-printf ; sudo iptables -P INPUT DROP; sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+printf "Aplicando reglas de iptables...\n"
+sudo iptables -P INPUT DROP
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
