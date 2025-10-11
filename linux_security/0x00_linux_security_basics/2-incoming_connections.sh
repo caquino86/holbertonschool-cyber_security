eca841bf566ca743d9ee89065a3b707543e55107
caquino86 <12019@holbertonstudents.com>
2025-10-11 19:05:25 -0400

    fix: add informative output to iptables rules application

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo iptables -P INPUT DROP; sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+printf "Aplicando reglas de iptables...\n"; sudo iptables -P INPUT DROP; sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
