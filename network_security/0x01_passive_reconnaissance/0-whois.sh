5fce79379aa89b8db584069369f2dcda45bea778
caquino86 <12019@holbertonstudents.com>
2025-10-23 09:08:15 -0400

    Update of the script

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F':' '/Registrant|Admin|Tech/ || /Street/ || /Phone Ext/ {gsub(/^ +| +$/,"",$2); printf "%s,%s%s\n",$1,$2,($1 ~ /Street/)?" ":""} /Phone Ext:/ && $2=="" {print "Phone Ext:,"}' > "$1.csv"
+whois "$1" | awk -F':' '/Registrant|Admin|Tech/ || /Street/ || /Phone Ext:/ {gsub(/^ +| +$/,"",$2); printf "%s,%s%s\n",$1,$2,($1 ~ /Street/)?" ":""}
