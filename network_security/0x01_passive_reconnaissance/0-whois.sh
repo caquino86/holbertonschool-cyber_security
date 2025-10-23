8cdd87392fcc412237594ef77c13a79fa424ef1b
caquino86 <12019@holbertonstudents.com>
2025-10-23 08:42:57 -0400

    fix: remove redundant awk command in 0-whois.sh for cleaner output

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F':' '/Registrant|Admin|Tech/ || /Street/ || /Phone Ext/ {gsub(/^ +| +$/,"",$2); printf "%s,%s%s\n",$1,$2,($1 ~ /Street/)?" ":""; if($1 ~ /Phone Ext/) && $2==""{printf "Phone Ext:,\n"}}' | awk 'ORS=NR==1?"":"\n"'
-
+whois "$1" | awk -F':' '/Registrant|Admin|Tech/ || /Street/ || /Phone Ext/ {gsub(/^ +| +$/,"",$2); printf "%s,%s%s\n",$1,$2,($1 ~ /Street/)?" ":""}'
