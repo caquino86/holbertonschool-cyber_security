631f1f7892cfb32b0cd486046fca8f50ea83daf5
caquino86 <12019@holbertonstudents.com>
2025-10-29 15:02:16 -0400

    Update del script.

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 whois "$1" | awk -F':' '/Registrant|Admin|Tech/ && /Name|Organization|Street|City|State|Postal Code|Country|Phone|Fax|Email/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $1 ":" ($2 ? $2 : "")}' | sed 's/:/:,/g' > "$1.csv"
-
