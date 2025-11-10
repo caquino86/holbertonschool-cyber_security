99143b39f8ef206a88e159b2fdc272e6914edc24
caquino86 <12019@holbertonstudents.com>
2025-11-10 09:14:12 -0500

    Script update

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$(nslookup "$1" | awk '/^Name:/ { name=$2 } /^Address: / { print name "," $2 }')"
+whois "$1" | awk '/Registrant|Admin|Tech/,/^$/ {gsub(/^Street:[[:space:]]*/, "Street,"); gsub(/^Phone Ext:[[:space:]]*/, "Phone Ext:,"); if ($0 ~ /^[A-Za-z ]+:/) {split($0,a,":"); v=substr($0,index($0,":")+1); gsub(/^[[:space:]]+/, "", v); printf "%s,%s", a[1], v; if ($0 ~ /^Street:/) printf " "; print ""}}' | sed '$!s/$/\n/'
