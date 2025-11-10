afc5303da2df1467066e05fea457593eb44a3bc3
caquino86 <12019@holbertonstudents.com>
2025-11-10 10:38:32 -0500

    f

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 whois "$1" | awk -F':' '/Registrant|Admin|Tech/ && /(Name|Organization|Street|City|State|Postal Code|Country|Phone$|Fax$|Email)/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); sub(/:$/, "", $1); print $1 "," $2} /Registrant|Admin|Tech/ && /(Phone Ext:|Fax Ext:)/ {print $1 ","}' > "$1.csv"
-
