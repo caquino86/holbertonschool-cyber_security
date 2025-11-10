3950e063094e0f89d2350205e195de8304a988e7
caquino86 <12019@holbertonstudents.com>
2025-11-10 09:20:57 -0500

    Script Update

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk '/Registrant|Admin|Tech/,/^$/ {gsub(/^Street:[[:space:]]*/, "Street,"); gsub(/^Phone Ext:[[:space:]]*/, "Phone Ext:,"); if ($0 ~ /^[A-Za-z ]+:/) {split($0,a,":"); v=substr($0,index($0,":")+1); gsub(/^[[:space:]]+/, "", v); printf "%s,%s", a[1], v; if ($0 ~ /^Street:/) printf " "; print ""}}' | sed '$!s/$/\n/'
+whois "$1" | awk '/Registrant|Admin|Tech/,/^$/ {f=$0; sub(/^[^:]+:/,""); gsub(/^[[:space:]]+/, "", f); if ($0 ~ /^Street:/) f=f" "; sub(/^([^:]+):/, "\\1,"); print f}' | sed '$!s/$/\n/'
