7994d807879ad4162d211d18de7d1549172c05db
caquino86 <12019@holbertonstudents.com>
2025-11-25 17:55:18 -0500

    Refactor 0-whois.sh to improve script structure and enhance error handling

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash; [ -z "$1" ] && { echo "Uso: $0 <dominio>" >&2; exit 1; } || { DOMAIN="$1"; OUTPUT_FILE="${DOMAIN}.csv"; }
-whois "$DOMAIN" | awk 'BEGIN{FS=":";OFS=",";T="^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\\/Province|Postal Code|Country|Phone|Email|Fax|Phone Ext|Fax Ext)$";E="(Phone Ext|Fax Ext)$";S=" Street$"} {gsub(/^[[:space:]]+|[[:space:]]+$/, "", $1);if(match($1,T)){F=$1;V="";for(i=2;i<=NF;i++){V=(i==2?"":V":") $i}gsub(/^[[:space:]]+|[[:space:]]+$/, "", V);if(F~E){F=F":"};if(F~S){V=V" "};print F,V}}' > "$OUTPUT_FILE"; exit 0
+#!/bin/bash
+whois "$1" | awk '/Registrant Information/{s="Registrant"} /Admin Information/{s="Admin"} /Tech Information/{s="Tech"} /^[A-Za-z].*:/{if(s!=""){split($0,a,":");f=a[1];v=substr($0,index($0,":")+1);gsub(/^ +| +$/,"",v);if(f~/Street/)v=v" ";if(f~/Ext$/)f=f":";printf("%s %s,%s\n",s,f,v)}}' | sed '$!s/$/\n/'
